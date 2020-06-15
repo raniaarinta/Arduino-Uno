b972cdb0e19f9b1a3f9650f6109539a884e703df
raniaarinta <64395618+raniaarinta@users.noreply.github.com>
2020-06-15 12:03:49 +0700

    Add files via upload

---
+++ four_lamp_virtual_button_blynk.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#define BLINK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+
+int Led=0,led1=13,led2=12,led3=14;
+
+
+int btnPin,btnpin2,btnpin3,btnpin4;
+
+char auth[]="221dbb17948e4fd8891f2249c0d5bf6f";
+
+char ssid[]="rania";
+char pass[]="rania123";
+
+BLYNK_WRITE(V1)
+{
+  btnPin = param.asInt();
+}
+BLYNK_WRITE(V2)
+{
+  btnpin2 = param.asInt();
+}  
+BLYNK_WRITE(V3)
+{
+  btnpin3 = param.asInt();
+} 
+BLYNK_WRITE(V4)
+{
+  btnpin4 = param.asInt();
+} 
+
+  
+
+void setup()
+{
+  pinMode(Led, OUTPUT); 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT); 
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+void loop()
+{
+  if(btnPin==0){
+    digitalWrite(Led, LOW);
+  }
+  else if(btnPin==1) {
+    digitalWrite(Led, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+  if(btnpin2==0){
+    digitalWrite(led1, LOW);
+  }
+  else if(btnpin2==1) {
+    digitalWrite(led1, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+  if(btnpin3==0){
+    digitalWrite(led2, LOW);
+  }
+  else if(btnpin3==1) {
+    digitalWrite(led2, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+  if(btnpin4==0){
+    digitalWrite(led3, LOW);
+  }
+  else if(btnpin4==1) {
+    digitalWrite(led3, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+  Blynk.run();
+}
