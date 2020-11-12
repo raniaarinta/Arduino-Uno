be265c49a7228bc3e67f64cedbfe64f5bf13c25b
raniaarinta <64395618+raniaarinta@users.noreply.github.com>
2020-11-12 10:43:53 +0700

    Add files via upload

---
+++ Ultrasonic_Sensor_with_Blynk/Ultrasonic_Sensor_with_Blynk.ino
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#define BLINK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+#define TRIGGERPIN D1
+#define ECHOPIN D2
+int led1=13,led2=12,led3=14;
+char auth[]="auth code";
+char ssid[]="wifi ssid";
+char pass[]="wifi pass";
+
+
+
+BlynkTimer timer;
+
+void setup()
+{ 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(3003L, sendSensor);
+
+}
+void loop()
+{
+ Blynk.run();
+ timer.run();
+  
+}
+
+void sendSensor()
+{
+  digitalWrite(TRIGGERPIN, LOW);   
+  delayMicroseconds(2);       
+
+  digitalWrite(TRIGGERPIN, HIGH);  
+  delayMicroseconds(10);      
+  digitalWrite(TRIGGERPIN, LOW);
+  
+  long duration,distance;   
+
+  duration = pulseIn(ECHOPIN, HIGH);   
+  distance = duration * 0.034 / 2;   
+
+  Serial.print("Distance = ");        //Output distance on arduino serial monitor
+  Serial.println(distance);
+  
+  if(distance <=5)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  }
+  else if(distance >20)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+  }
+   else if(distance >40)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+  }
+  Blynk.virtualWrite(V0, distance);
+  delay(1000);                       
+}
---
+++ Ultrasonic_Sensor_with_email_notification_Blynk/Ultrasonic_Sensor_with_email_notification_Blynk.ino
@@ -0,0 +1,59 @@
+#include <ESP8266WiFi.h>
+#define BLINK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+#define TRIGGERPIN D1
+#define ECHOPIN D2
+
+char auth[]="auth code";
+char ssid[]="wifi ssid";
+char pass[]="wifi pass";
+char email_address[]="";
+char email_subjects[]="";
+char email_message[]="";
+
+
+BlynkTimer timer;
+
+void setup()
+{
+  
+
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(3003L, sendSensor);
+
+}
+void loop()
+{
+ Blynk.run();
+ timer.run();
+  
+}
+
+void sendSensor()
+{
+  digitalWrite(TRIGGERPIN, LOW);   
+  delayMicroseconds(2);       
+
+  digitalWrite(TRIGGERPIN, HIGH);  
+  delayMicroseconds(10);      
+  digitalWrite(TRIGGERPIN, LOW);
+  
+  long duration,distance;   
+
+  duration = pulseIn(ECHOPIN, HIGH);   
+  distance = duration * 0.034 / 2;   
+
+  Serial.print("Distance = ");        //Output distance on arduino serial monitor
+  Serial.println(distance);
+  
+  if(distance <=20)
+  {
+    Blynk.email(email_address,email_subjects,email_message);
+    
+  }
+  Blynk.virtualWrite(V0, distance);
+  delay(1000);                       
+}
